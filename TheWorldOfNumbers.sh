70c9cc0becd5ce0fdbba4d393d4aed85aef94c2e
Bhavya Akhil Saraf <bhavya.sa@MMMDVBHAVYASA.local>
2021-07-10 19:05:13 +0530

    Solution The World of Numbers

---
+++ TheWorldOfNumbers.sh
@@ -0,0 +1,8 @@
+#Link: https://www.hackerrank.com/challenges/bash-tutorials---the-world-of-numbers/problem?h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
+
+read num1
+read num2
+echo `expr $num1 + $num2`
+echo `expr $num1 - $num2`
+echo `expr $num1 \* $num2`
+echo `expr $num1 / $num2`
